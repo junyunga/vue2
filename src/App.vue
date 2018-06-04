e2990f7b56ef3b1d18fc5aa2f2a84e78bcb0b812
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-06-04 17:33:45 +0900

    기초

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,25 @@
 <template>
-  <div>
-    <input v-model="msg">
-    <p>prop: {{propMessage}}</p>
-    <p>msg: {{msg}}</p>
-    <p>helloMsg: {{helloMsg}}</p>
-    <p>computed msg: {{computedMsg}}</p>
-    <button @click="greet">Greet</button>
+  <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import Component from 'vue-class-component'
-
-@Component({
-  props: {
-    propMessage: String
+  import Vue from 'vue'
+  
+  
+  export default class App extends Vue {
+  
   }
-})
-export default class App extends Vue {
-  // initial data
-  msg = 12023
-
-  // use prop values for initial data
-  helloMsg = 'Hello, ' + this.propMessage
-
-  // lifecycle hook
-  mounted () {
-    this.greet()
-  }
-
-  // computed
-  get computedMsg () {
-    return 'computed ' + this.msg
-  }
-
-  // method
-  greet () {
-    alert('greeting: ' + this.msg)
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
