07fbc6a94c140cf5a178fff8cdea0e48943ca694
JunYoung-Cho <cjy8745@ilts.co.kr>
2018-06-06 18:55:44 +0900

    ㅇㅇ

---
+++ src/components/layout/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Header</div>
+</template>
+<script>
+    import {
+        Component,
+        Vue
+    } from 'vue-property-decorator'
+    @Component
+    export default class Header extends Vue{
+        
+    }
+</script>
+<style lang="">
+    
+</style>
--- src/main.vue
+++ src/main.vue
@@ -1,9 +1,25 @@
-<template> 
-  
+<template>
+    <div>
+        <aaa></aaa>
+    </div>
 </template>
-<script>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import Component from 'vue-class-component'
+    import Header from './components/layout/header.vue'
     
+    
+    @Component({
+        components: {
+            "aaa": Header
+        }
+    })
+    export default class Main extends Vue {
+    
+    }
 </script>
+
 <style lang="">
     
 </style>
